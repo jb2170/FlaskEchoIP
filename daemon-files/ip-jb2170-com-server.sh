8120ccb72200996366acefa39f1443853b81983f
jb2170 <email@jb2170.com>
2026-01-18 06:21:23 +0000

    Update daemon-files: better shebang, source venv, and use relative gunicorn

--- daemon-files/ip-jb2170-com-server.sh
+++ daemon-files/ip-jb2170-com-server.sh
@@ -1,6 +1,8 @@
-#!/bin/bash -eu
+#!/usr/bin/env -S bash -eu
 
-exec ${HOME}/.local/var/lib/FlaskEchoIP/venv/bin/gunicorn \
+source ./venv/bin/activate
+
+exec gunicorn \
 -b unix:/public${HOME}/sockets/ip-jb2170-com-server \
 --log-file - \
 FlaskEchoIP:app
